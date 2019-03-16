295f7d1c28dee3ad113e9f27a8399ac653a972a0
Yurii <masteryada@Yuriis-MacBook-Pro.local>
2019-03-16 17:53:23 +0200

    move is ready

--- js/script.js
+++ js/script.js
@@ -14,29 +14,35 @@ var scoreEl = document.getElementById('score');
 var lastStrike = Date.now();
 var lastTime;
 var score = 0;
+var timeBlock;
+var action;
 var img = new Image();
 img.src = 'img/my_background_2.png';
 var hero = new Image();
 //hero.src = 'img/silver.png';
-hero.src ="img/playerMoves.png";
+hero.src ="img/player.png";
 var enemy = new Image();
-enemy.src = 'img/enemy_moves.png'
+enemy.src = 'img/enemy.png'
 var lastFrameTime = 0;
 resources = {
     images:{
        // "img/silver.png": hero,
-        "img/playerMoves.png": hero,
-        'img/enemy_moves.png': enemy
+        "img/player.png": hero,
+        'img/enemy.png': enemy
     },
     get(url){
         return this.images[url];
     }
 }
  var moves ={
-     run: new Sprite('img/playerMoves.png', [20, 220], [127, 180], 17, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16]),
-     jump: new Sprite('img/playerMoves.png', [20, 415], [93, 150], 27, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16,17,18,19,20,21,22,23,24,25,26,27]),
-     hit: new Sprite('img/playerMoves.png', [12, 575], [164.5, 165], 17, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16])
-     //hit: new Sprite('img/playerMoves.png', [0, 25], [192.16, 170], 25, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49])
+     run: ['img/player.png', [40, 621], [127, 180], 17, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16]],
+     jump: ['img/player.png', [40, 809], [93, 150], 27, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16,17,18,19,20,21,22,23,24,25,26,27]],
+     hit: ['img/player.png', [17, 974], [164.5, 165], 17, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16]],
+     block: ['img/player.png', [20, 15], [91.2, 165], 18, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16,17]],
+     walk: ['img/player.png', [20, 195], [112.4, 180], 25, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16,17,18,19,20,21,22,23,24]],
+     walk_LB: ['img/player.png', [20, 195], [112.4, 180], 25, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16,17,18,19,20,21,22,23,24].reverse()]
+
+     //hit: new Sprite('img/player.png', [0, 25], [192.16, 170], 25, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49])
 
  } 
 
@@ -44,13 +50,19 @@ resources = {
 // Game state
 var player = {
     pos: [100, 800],
-    sprite: new Sprite('img/playerMoves.png', [20, 220], [127, 180], 17, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16]),
-    action: 'run'
+   // sprite: new Sprite('img/player.png', [30, 220], [127, 180], 17, [0, 1, 2, 3 ,4 ,5, 6, 7,8,9,10,11,12,13,14 ,15,16]),
+    sprite: new Sprite(...moves.walk),
+    action: 'walk'
 };
 var enemyMove = {
-    walk: ['img/enemy_moves.png', [15, 12], [153, 156], 25, [0, 1, 2, 3 ,4 ,5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24].reverse()],
-    hurt: ['img/enemy_moves.png', [0, 0], [153, 156], 25, [0, 1, 2, 3 ,4 ,5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24].reverse()],
-    death:new Sprite('img/enemy_moves.png', [0, 365], [190, 156], 25, [0, 1, 2, 3 ,4 ,5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35].reverse())
+    walk:     ['img/enemy.png', [18, 12], [153, 156], 25, [0, 1, 2, 3 ,4 ,5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24].reverse()],
+    hurt: [
+            ['img/enemy.png', [23.5, 781], [212, 170], 14, [0, 1, 2, 3 ,4 ,5, 6, 7, 8,9,10,11,12,13].reverse()],
+            ['img/enemy.png', [40, 984], [240, 170], 21, [0, 1, 2, 3 ,4 ,5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20].reverse()],
+            ['img/enemy.png', [40, 1187], [178, 170], 33, [0, 1, 2, 3 ,4 ,5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32].reverse()]
+        ],
+    happy: new Sprite('img/enemy.png', [23.5, 585], [81, 156], 21, [0, 1, 2, 3 ,4 ,5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20].reverse()),
+    death: ['img/enemy.png', [0, 365], [190, 156], 25, [0, 1, 2, 3 ,4 ,5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35].reverse()]
 } 
 var timeOut = null;
 var enemies = [];
@@ -159,7 +171,8 @@ function update(dt) {
             enemies.push({
                 pos: [canvas.width-20, 810],
                 sprite: new Sprite (...enemyMove.walk),
-                action: 'walk'
+                action: 'walk',
+                block: false
             });
         
             clearTimeout(timerNow);
@@ -178,31 +191,49 @@ function update(dt) {
 function handleInput(dt) {
     var heigth = 100;
     function animationDown(){
-        timer = setInterval(function(){
+       let timer = setInterval(function(){
             player.pos[1] +=5;
             if(input.isDown('RIGHT') || input.isDown('d'))
             {player.pos[0] +=1;}
             if(player.pos[1] == 800){
                 clearInterval(timer);
-                player.sprite = moves.run;
-                player.action = 'run';
+                player.sprite = new  Sprite(...moves.walk);
+                player.action = 'walk';
             }
         }, 6)
     }
 
-    if(input.isDown('DOWN') || input.isDown('s')) {
-       // player.pos[1] += playerSpeed * dt;
-       
+    if(input.isDown('DOWN') || input.isDown('s') && player.action !='jump') {
+    //     console.log('timeBlock', timeBlock);
+    //     if(player.action != 'block'){
+    //         action = player.action; 
+    //         timeBlock = new Date().getTime();
+    //         player.sprite = new Sprite(...moves.block);
+    //         player.action = 'block';
+    //     }else if(new Date().getTime() >= timeBlock+300) { 
+    //         player.sprite.frames = [10];
+    //     }
+    //   //  let timer = setTimeout(function(){
+    //     //        clearTimeout(timer);
+    //      //       player.sprite = new  Sprite(...moves.walk);
+    //    //         player.action = 'walk';
+    //   //  }, 1000)
+    // }else if(action && player.action!=action) {
+    //     player.action = action;
+    //     player.sprite = new Sprite(...moves[action]);
+    //     console.log('action', action);
+    //     console.log('stopDown');
     }
+    
 
     //-jump
     
     if(input.isDown('UP') || input.isDown('w')) {
         console.log(player.pos[1]);
         if(player.pos[1] == 800) {
-            player.sprite = moves.jump;
+            player.sprite = new Sprite(...moves.jump);
             player.action = 'jump';
-            timer = setInterval(function(){
+            let timer = setInterval(function(){
                 player.pos[1] -=5;
                 if(input.isDown('RIGHT') || input.isDown('d'))
                 {player.pos[0] +=1;}
@@ -216,22 +247,30 @@ function handleInput(dt) {
 
     if(input.isDown('LEFT') || input.isDown('a')) {
         player.pos[0] -= playerSpeed * dt;
-      
+        if(player.action != 'walk_LB'){   
+            console.log('player.action', player.action) 
+                player.sprite = new Sprite(...moves.walk_LB);
+                player.action = 'walk_LB';
+        }
     }
 
     if(input.isDown('RIGHT') || input.isDown('d')) {
         player.pos[0] += playerSpeed * dt;
-       
+        if(player.action != 'run'){   
+        console.log('player.action', player.action);    
+            player.sprite = new  Sprite(...moves.run);
+            player.action = 'run';
+        }
     }
     //-hit
     if(input.isDown('SPACE') &&
        !isGameOver &&
        Date.now() - lastStrike> 2000) {
-        player.sprite = moves.hit;
+        player.sprite = new  Sprite(...moves.hit);
         player.action = 'hit';
         setTimeout(function(){
-            player.sprite = moves.run;
-            player.action = 'run';
+            player.sprite = new  Sprite(...moves.walk);
+            player.action = 'walk';
         },1000)
         lastStrike = Date.now();
     }
@@ -251,6 +290,28 @@ function boxCollides(pos, size, pos2, size2) {
                     pos2[0] + size2[0], pos2[1] + size2[1]);
 }
 
+function checkRandomPosition(player, enemy){
+    let randomPos = randomInteger(4,9) * 10;
+    let randomHurt = randomInteger(0,2);
+    let pos = player.pos,
+        size = player.sprite.size,
+        pos2 = enemy.pos;
+
+    if(pos[0]+size[0]+randomPos >= pos2[0] ){
+        enemy.block = true;
+        enemy.sprite = new Sprite(...enemyMove.hurt[randomHurt]);
+        enemy.action = 'hurt';
+        setTimeout(() => {
+            if(enemy && enemy.action!='death'){
+                enemy.block = false;
+                enemy.action = 'walk';
+                enemy.sprite = new Sprite(...enemyMove.walk);
+                console.log(false, player.pos)
+            }
+        }, 800);
+        console.log(randomPos);
+    }
+}
 
 function checkPlayer(){
     if(player.pos[0] < 0) {
@@ -269,17 +330,21 @@ function checkCollisions() {
     for(let enemy of enemies){
         var pos = enemy.pos;
         var size = enemy.sprite.size;
- 
-            var pos2 = [player.pos[0]+50,player.pos[1]];        
-            var size2 = [20,50]
+        var pos2 = [player.pos[0]+50,player.pos[1]];        
+        var size2 = [20,50];
 
-            if(boxCollides(pos, size, pos2, size2) && player.action == 'hit'&&!timeOut && enemy.action!='death') {
+        if(!enemy.block){
+            checkRandomPosition(player, enemy);
+
+        }
+
+        if(boxCollides(pos, size, pos2, size2) && player.action == 'hit'&&!timeOut && enemy.action!='death') {
                 // Remove the enemy
                 enemy.action = 'death';
-                enemy.sprite = enemyMove.death;
+                enemy.sprite = new Sprite(...enemyMove.death);
                 timeOut = setTimeout(() => {
                 
-                 enemy.sprite = new Sprite('img/enemy_moves.png', [0, 365], [190, 156], 1, [0])
+                 enemy.sprite = new Sprite('img/enemy.png', [0, 365], [190, 156], 1, [0])
                  score += 100;
                  clearTimeout(timeOut);
                  timeOut = null;
@@ -292,7 +357,7 @@ function checkCollisions() {
                 // Remove the bullet and stop this iteration
                 break;
             }else if (enemy.action == 'death' && boxCollides(pos, size, pos2, size2)){
-                
+                //--здвиг
             }
             else if(player.action != 'hit' && boxCollides(pos, size, pos2, size2)){
                 gameOver();
@@ -325,6 +390,9 @@ function render() {
     // Render the player if the game isn't over
     if(!isGameOver) {
         renderEntity(player);
+        
+    }else{
+        player.pos = [-Infinity,-Infinity];
     }
     
         renderEntities(enemies);
